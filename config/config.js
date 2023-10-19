5b46954d18db99103bed1d9c6c5640d23be5ab3f
ChangSol <ckdwnsla12@naver.com>
2023-10-19 23:37:30 +0900

    github-actions11

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const prefix =
+  process.env.NODE_ENV === "production"
+    ? "http://changsol.github.io/wedding-invitation"
+    : "";
--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
-}
+const debug = process.env.NODE_ENV !== "production";
+const repository = "http://changsol.github.io/wedding-invitation";
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+};
+
+module.exports = nextConfig;
