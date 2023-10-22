5f213b7c032a753b026450bb4a0d8f8f035caf44
ChangSol <ckdwnsla12@naver.com>
2023-10-22 18:56:02 +0900

    github-actions update 231022-1

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://changsol.github.io/wedding-invitation";
+
 const nextConfig = {
+  // distDir: 'build',
+  // assetPrefix: '.',
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   swcMinify: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
+  // env: {
+  //   BASE_URL: process.env.BASE_URL,
+  // },
 }
 
 module.exports = nextConfig
