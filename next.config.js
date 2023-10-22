bf9b2fce3eb752381c25b32ec10ab05ce2c3ee10
ChangSol <ckdwnsla12@naver.com>
2023-10-22 22:01:59 +0900

    github-actions update 231022-8

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const repository = ".";
 const nextConfig = {
   // distDir: 'build',
   // assetPrefix: '.',
+  basePath: '/wedding-invitation',
   reactStrictMode: true,
   assetPrefix: !debug ? `${repository}` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
