90e8414f9cdf58df4683296da6ab0fa0ad3b026d
ChangSol <ckdwnsla12@naver.com>
2023-10-22 23:26:02 +0900

    _document.tsx og remove

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,17 +6,6 @@ export default class _Document extends Document {
       <Html>
         <Head>
           <script defer src="https://developers.kakao.com/sdk/js/kakao.js" />
-          <meta property="og:url" content="https://github.com/changsol" />
-          <meta property="og:title" content="창주❣️신희 결혼합니다" />
-          <meta property="og:type" content="website" />
-          <meta
-            property="og:image"
-            content="https://github.com/changsol/pictures/a10.jpg"
-          />
-          <meta
-            property="og:description"
-            content="24년 02월 24일 (토) 오후 1시 \n채림웨딩홀 6층 컨벤션홀"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
