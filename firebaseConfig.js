d150213a9d6b8c048518bed902ab1a78c8fc90ff
ChangSol <ckdwnsla12@naver.com>
2023-11-10 14:36:24 +0900

    fix env public name add

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY_1,
-  authDomain: process.env.FB_AUTH_DOMAIN_1,
-  projectId: process.env.FB_PROJECT_ID_1,
-  storageBucket: process.env.FB_STORAGE_BUCKET_1,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID_1,
-  appId: process.env.FB_APP_ID_1,
+  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY_1,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN_1,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID_1,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET_1,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID_1,
+  appId: process.env.NEXT_PUBLIC_FB_APP_ID_1,
 };
 
 export default firebaseConfig;
